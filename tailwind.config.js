2299d8a0a98935738a5d1cef5ba4b7ed0b120be8
Ali <9.alialipour.0@gmail.com>
2024-01-16 18:21:40 +0330

    Improve Style Logo Block

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
       width: {
         '128': '32rem',
         '384': '99rem',
+        '141.34' : '141.34px;'
       },
+      height: {
+        '50': '50px',
+      },
+      colors: {
+        'NestMart-Brand-1': '#3BB77E',
+        'NestMart-TextHeading': '#253D4E',
+      },
+      fontFamily: {
+        'logo-font': ['Quicksand', 'sans-serif'],
+        'Montserrat': ['Montserrat', 'sans-serif'],
+      },
+      lineHeight: {
+        '12': '3rem',
+      },
+      letterSpacing: {
+        logo: '-0.019px',
+      }
       
     },
   },
