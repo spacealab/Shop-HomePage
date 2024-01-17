6455caf47152602144de87a060fed33c658c0824
Ali <9.alialipour.0@gmail.com>
2024-01-17 15:24:16 +0330

    Improve Style Account

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       fontFamily: {
         'logo-font': ['Quicksand', 'sans-serif'],
         'Montserrat': ['Montserrat', 'sans-serif'],
+        'Lato': ['Lato', 'sans-serif'],
       },
       lineHeight: {
         '12': '3rem',
