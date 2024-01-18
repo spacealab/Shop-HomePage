5b7e06eda7404d9db765ac8ff9d4eec6eb7ea725
Ali Alipour <9.alialipour.0@gmail.com>
2024-01-19 00:46:57 +0330

    Improve Style to Hero

---
+++ src/scripts/script.js
@@ -0,0 +1,21 @@
+  var mySwiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If you need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if you need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
