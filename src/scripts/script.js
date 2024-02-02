c3f1aa2af89000a6fac1f3e7a758508a66e19efd
Ali Alipour <spacealab@gmail.com>
2024-02-02 14:59:39 +0330

    make Menu and improve style

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,17 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    console.log("ok"); // Log 'ok' when the menu is shown
+  } else {
+    menu.classList.add("hidden");
+  }
+};
+
+
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
   },
 });
+
